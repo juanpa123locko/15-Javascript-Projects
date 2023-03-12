a9266c0b46c804a7056e9e7a5ca52e0dda1bd26b
Juan Pablo <juanpa_roto@hotmail.com>
2023-03-11 23:29:49 -0600

    linked 3 projects of 15

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const btnnav = document.querySelector('.navbar-burger');
 const navmenu = document.querySelector('.menu');
 const body = document.querySelector('body');
+const links = document.querySelectorAll('.menu a');
 btnnav.addEventListener('click', () => {
     body.classList.toggle('open');
-console.log('click');
+
+});
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        body.classList.remove('open');
+    });
 });
