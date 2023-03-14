c0a97d91736987ba475a2d7a0dbe6ab9da53523a
Juan Pablo <juanpa_roto@hotmail.com>
2023-03-14 09:56:56 -0600

    ended the navbar part and added the button to top page

--- main.js
+++ main.js
@@ -14,10 +14,22 @@ links.forEach(link => {
 // navbar transparent when scroll down
 const navbar = document.querySelector('.navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarburger = document.querySelector('.navbar-burger');
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar-transparent');
+        navbarburger.classList.add('navbar-burger-dark');
     } else {
         navbar.classList.remove('navbar-transparent');
+        navbarburger.classList.remove('navbar-burger-dark');
+    }
+});
+
+const backtotopbutton = document.querySelector('.backtotopbutton');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 600) {
+        backtotopbutton.classList.add('visiblebutton');
+    } else {
+        backtotopbutton.classList.remove('visiblebutton');
     }
 });
